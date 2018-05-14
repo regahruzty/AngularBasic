05ecd51a87e101ef9bf7fff94b78f20341a8995d
Rega <rega@collega.com>
2018-05-14 10:22:58 +0700

    componentdetail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BP_PREFIX } from 'blocking-proxy/built/lib/blockingproxy';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Aplikasi Perpustakaan';
+
+  books: Book[]=[
+    ];
+
+  book : Book = new Book();
+
+  kirimData(data){
+    console.log(this.book);
+    this.books.push(this.book);
+    this.book = new Book();
+  }
+}
+
+export class Book{
+  constructor(public name?: string, public tahunTerbit?: number){}
 }
